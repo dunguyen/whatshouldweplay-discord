0cb1d43757a8e7da00e4459c664112f1fc00d5ba
Du Nguyen <dunguyen90@gmail.com>
2020-05-12 12:13:52 +0200

    Remove deprecated tslint and replace with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module"
+    },
+    env: {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:node/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ]
+};
