c96e5e10c353a9c0a8e00411a412cde881829c52
Du Nguyen <dunguyen90@gmail.com>
2020-06-01 12:45:10 +0200

    Fix test

--- test/commands/help.test.ts
+++ test/commands/help.test.ts
@@ -20,6 +20,6 @@ describe('help command tests', () => {
         const args: string[] = ['help'];
 
         await helpCommand.execute(message, args);
-        expect(message.sendDM).toHaveBeenCalledTimes(1);
+        expect(message.reply).toHaveBeenCalledTimes(1);
     });
 });
