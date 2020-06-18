a5d86127f1963e0013f96d8136c9964079bb3873
Du Nguyen <dunguyen90@gmail.com>
2020-06-18 11:44:14 +0200

    Delete command

--- src/util/commands.ts
+++ src/util/commands.ts
@@ -8,6 +8,7 @@ import { UnlinkCommand } from '../commands/unlink';
 import { ShowLinkedCommand } from '../commands/showlinked';
 import { UpdateLinkedCommand } from '../commands/updatelinked';
 import { PruneCommand } from '../commands/prune';
+import { DeleteCommand } from '../commands/delete';
 
 export const getCommands = function (): Discord.Collection<string, ICommand> {
     const commands = new Discord.Collection<string, ICommand>();
@@ -19,6 +20,7 @@ export const getCommands = function (): Discord.Collection<string, ICommand> {
     const showLinkedCommand = new ShowLinkedCommand();
     const updateLinkedCommand = new UpdateLinkedCommand();
     const pruneCommand = new PruneCommand();
+    const deleteCommand = new DeleteCommand();
 
     commands.set(helpCommand.name, helpCommand);
     commands.set(playCommand.name, playCommand);
@@ -27,6 +29,7 @@ export const getCommands = function (): Discord.Collection<string, ICommand> {
     commands.set(showLinkedCommand.name, showLinkedCommand);
     commands.set(updateLinkedCommand.name, updateLinkedCommand);
     commands.set(pruneCommand.name, pruneCommand);
+    commands.set(deleteCommand.name, deleteCommand);
 
     return commands;
 };
