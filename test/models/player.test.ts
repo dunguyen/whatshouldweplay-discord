c5a246b7b05a13f97f99b8fd0e2f56bbc79d2389
Du Nguyen <dunguyen90@gmail.com>
2020-07-05 13:08:36 +0200

    Remove broken test

--- test/models/player.test.ts
+++ test/models/player.test.ts
@@ -1,6 +1,4 @@
 import { Player } from '../../src/models/player';
-import * as Request from '../../src/util/request';
-import * as Game from '../../src/models/game';
 
 jest.mock('../../src/util/request');
 jest.mock('../../src/models/game');
@@ -40,50 +38,5 @@ describe('player tests', () => {
 
         expect(userId).toBe('steam id/<@discord>');
         expect(hasGames).toBeFalsy();
-    });
-
-    test('player populate steam games', () => {
-        const player = new Player('steam id');
-        const userId = player.getUserIds();
-        const hasGames = player.hasGames();
-
-        const getSteamIdMock = jest
-            .spyOn(Request, 'getSteamId')
-            .mockImplementation((username) => {
-                return new Promise((resolve) => {
-                    resolve('1234');
-                });
-            })
-            .mockClear();
-
-        const getOwnedSteamGamesMock = jest
-            .spyOn(Request, 'getOwnedSteamGames')
-            .mockImplementation((id) => {
-                return new Promise((resolve) => {
-                    resolve({
-                        success: true,
-                        steamAppIds: [
-                            { appId: 1, playtime: 0 },
-                            { appId: 2, playtime: 0 },
-                            { appId: 3, playtime: 0 },
-                            { appId: 4, playtime: 0 },
-                        ],
-                        id: id,
-                    });
-                });
-            })
-            .mockClear();
-
-        const gameFindMock = jest
-            .spyOn(Game, 'find')
-            .mockImplementation((filter) => {
-                return new Promise((resolve) => {
-                    resolve([new GameDocument(), new GameDocument(), new GameDocument(), new GameDocument()]);
-                });
-            })
-            .mockClear();
-
-        expect(userId).toBe('steam id');
-        expect(hasGames).toBeTruthy();
     });
 });
